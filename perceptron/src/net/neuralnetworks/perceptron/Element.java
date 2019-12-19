dd19fbeb605ca48e1f22581e0416258fcfcd0e3e
Timon Schmid <timon.schmid@bluewin.ch>
2019-12-19 23:42:30 +0100

    Perceptron

--- perceptron/src/net/neuralnetworks/perceptron/Element.java
+++ perceptron/src/net/neuralnetworks/perceptron/Element.java
@@ -1,44 +1,50 @@
 package net.neuralnetworks.perceptron;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Element {
 
-    private String category;
-    private double[] weights;
+    double[] w; // weights.
 
-    public Element(final int amountOfEntries) {
-        weights = new double[amountOfEntries];
-        Arrays.fill(weights, 1);
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void train(double[] entries, double isCorrectCategory) {
-        for (int i = 0; i < weights.length; i++) {
-            weights[i] = learnMultiplier(entries) * entries[i] * (isCorrectCategory - calcCorrectCategory(entries));
+    public Element(int numberOfWeights) {
+        w = new double[numberOfWeights];
+        for (int i = 0; i < numberOfWeights; i++) {
+            w[i] = 1.0;
         }
     }
 
-    public double calcCorrectCategory(double[] entries) {
-        double sumWiXi = 0d;
-        for (int i = 0; i < weights.length; i++) {
-            sumWiXi += weights[i] * entries[i];
+    private int theta(double value) {
+        if (value > 0.0001) {
+            return 1;
+        } else {
+            return 0;
         }
-        return sumWiXi >= 0 ? 1d : 0d;
     }
 
-    private double learnMultiplier(double[] entries) {
-        double sumWiXi = 0d;
-        double sumXiXi = 0d;
-        for(int i = 0; i < weights.length; i++) {
-            sumWiXi = weights[i] * entries[i];
-            sumXiXi = entries[i] * entries[i];
+    public int reaction(List<Integer> x) {
+        double y = 0;
+        for (int i = 0; i < w.length; i++) {
+            y += w[i] * x.get(i);
         }
-        return sumWiXi / sumXiXi;
+        return theta(y);
     }
 
+    private double e(List<Integer> x) {
+        double dividend = 0;
+        double divisor = 0;
+        for (int i = 0; i < w.length; i++) {
+            dividend += w[i] * x.get(i);
+            divisor += Math.pow(x.get(i), 2);
+        }
+        return dividend / divisor;
+    }
 
+    public void learn(List<Integer> pattern, int C) {
+        int y = reaction(pattern);
+        if (y != C) {
+            for (int i = 0; i < w.length; i++) {
+                w[i] += e(pattern) * pattern.get(i) * (C - y);
+            }
+        }
+    }
 }
--- perceptron/src/net/neuralnetworks/perceptron/Perceptron.java
+++ perceptron/src/net/neuralnetworks/perceptron/Perceptron.java
@@ -1,24 +1,48 @@
 package net.neuralnetworks.perceptron;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Perceptron {
 
-    private Element[] elements;
-    private int entriesPerElement;
+    private List<List<Integer>> trainingPatterns = new ArrayList<>();
 
-    public Perceptron(final int amountOfElements, final int entriesPerElement) {
-        elements = new Element[amountOfElements];
-        for (int i = 0; i < amountOfElements; i++) {
-            elements[i] = new Element(entriesPerElement);
-        }
-        this.entriesPerElement = entriesPerElement;
+    private List<Integer> patternElementCategory = new ArrayList<>();
+    private List<Element> elements = new ArrayList<>();
+
+    private int numberOfWeights;
+
+    public Perceptron(int numberOfWeights) {
+        this.numberOfWeights = numberOfWeights;
     }
 
-    public void trainElements(double[] entries, String category, int trainingIterations) {
-        for (int i = 0; i < trainingIterations; i++) {
-            for (Element element : elements) {
-                double isCorrectCategory = element.getCategory().equals(category) ? 1d : 0d;
-                element.train(entries, isCorrectCategory);
+    public void addElement() {
+        elements.add(new Element(numberOfWeights));
+    }
+
+    public void addPattern(List<Integer> pattern, Integer category) {
+        trainingPatterns.add(pattern);
+        patternElementCategory.add(category);
+    }
+
+    private void train() {
+        for (int element = 0; element < elements.size(); element++) {
+            for (int pattern = 0; pattern < trainingPatterns.size(); pattern++) {
+                int category = 0;
+                if (element == patternElementCategory.get(pattern)) {
+                    category = 1;
+                }
+                elements.get(element).learn(trainingPatterns.get(pattern), category);
             }
         }
     }
+
+    private List<Integer> useModel(List<Integer> pattern) {
+        List<Integer> result = new ArrayList<>();
+        for (Element element : elements) {
+            result.add(element.reaction(pattern));
+        }
+        return result;
+    }
+
 }
